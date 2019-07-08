58ea2f48ba8baa9f35fddc20cf9aa63d20a5cab5
Kunto Aji. K <kuntoaji@kaklabs.com>
2019-07-08 22:19:22 +0700

    v0.2.0

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+0.2.0:
+  * Show information when git is not available
+  * kuy command accepts first argument as branch name
+
+0.1.0:
+ * Initial gem release
+ * kuy command for pulling and merging from origin/develop branch
+ * kuymaster command for pulling and merging from origin/master branch
--- lib/kuy.rb
+++ lib/kuy.rb
@@ -1,12 +1,14 @@
 class Kuy
   def self.pull_and_merge_from(branch)
+    return 'git is not installed' if system('git --version').nil?
+
     current_branch = `git rev-parse --abbrev-ref HEAD`.strip
 
-    puts "checkout and update from #{branch}"
+    puts "Running checkout and update from origin/#{branch}"
     `git checkout #{branch}`
     `git pull origin #{branch}`
 
-    puts "merging with #{current_branch}"
+    puts "Merging with origin/#{current_branch}"
     `git checkout #{current_branch}`
     `git merge #{branch} --no-ff`
     puts 'Done!'
